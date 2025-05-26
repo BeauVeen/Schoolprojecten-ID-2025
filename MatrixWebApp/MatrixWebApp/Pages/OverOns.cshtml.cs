3be186ed617d92ecbaa2339d95d5724abb6099aa
BeauVeen <beauvdveen@pm.me>
2025-05-26 20:23:34 +0200

    modified:   MatrixWebApp/MatrixWebApp.sln 	new file:   MatrixWebApp/MatrixWebApp/Pages/Account.cshtml 	new file:   MatrixWebApp/MatrixWebApp/Pages/Account.cshtml.cs 	renamed:    MatrixWebApp/MatrixWebApp/Pages/Admin.cshtml -> MatrixWebApp/MatrixWebApp/Pages/Admin-panel/Admin.cshtml 	renamed:    MatrixWebApp/MatrixWebApp/Pages/Admin.cshtml.cs -> MatrixWebApp/MatrixWebApp/Pages/Admin-panel/Admin.cshtml.cs 	new file:   MatrixWebApp/MatrixWebApp/Pages/Bestellingen.cshtml 	new file:   MatrixWebApp/MatrixWebApp/Pages/Bestellingen.cshtml.cs 	modified:   MatrixWebApp/MatrixWebApp/Pages/Index.cshtml 	new file:   MatrixWebApp/MatrixWebApp/Pages/Login.cshtml 	new file:   MatrixWebApp/MatrixWebApp/Pages/Login.cshtml.cs 	new file:   MatrixWebApp/MatrixWebApp/Pages/Logout.cshtml 	new file:   MatrixWebApp/MatrixWebApp/Pages/Logout.cshtml.cs 	new file:   MatrixWebApp/MatrixWebApp/Pages/OverOns.cshtml 	new file:   MatrixWebApp/MatrixWebApp/Pages/OverOns.cshtml.cs 	modified:   MatrixWebApp/MatrixWebApp/Pages/Shared/_Layout.cshtml 	new file:   MatrixWebApp/MatrixWebApp/Pages/Winkelmand.cshtml 	new file:   MatrixWebApp/MatrixWebApp/Pages/Winkelmand.cshtml.cs 	modified:   MatrixWebApp/MatrixWebApp/wwwroot/css/site.css

---
+++ MatrixWebApp/MatrixWebApp/Pages/OverOns.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MatrixWebApp.Pages
+{
+    public class OverOnsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
